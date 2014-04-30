4159d664630d6b1dd3eafeb908bac7007df7be91
Oleg Kalugin <olegspace@gmail.com>
2014-04-29 20:40:43 -0700

    Switch to RemoteMuzeiArtSource for proper photo retrieval.

--- app/src/main/java/com/olegkalugin/muzei/gopro/GoProPotdArtSource.java
+++ app/src/main/java/com/olegkalugin/muzei/gopro/GoProPotdArtSource.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 
 import com.google.android.apps.muzei.api.Artwork;
-import com.google.android.apps.muzei.api.MuzeiArtSource;
+import com.google.android.apps.muzei.api.RemoteMuzeiArtSource;
 
-public class GoProPotdArtSource extends MuzeiArtSource {
+public class GoProPotdArtSource extends RemoteMuzeiArtSource {
     private static final String TAG = "GoProPOTD";
     private static final String SOURCE_NAME = "GoProPotdArtSource";
 
@@ -23,15 +23,18 @@ public class GoProPotdArtSource extends MuzeiArtSource {
     }
 
     @Override
-    protected void onUpdate(int reason) {
+    protected void onTryUpdate(int reason) throws RetryException {
         GoProService goProService = new GoProService();
         GoProService.Photo photo = goProService.getPhoto();
+
+        if (photo == null || photo.uri == null) {
+            throw new RetryException();
+        }
 
         publishArtwork(new Artwork.Builder()
                 .title(photo.title)
                 .byline(photo.byline)
                 .imageUri(photo.uri)
-                .token("goprotoken")
                 .viewIntent(new Intent(Intent.ACTION_VIEW, Uri.parse(GoProService.WEBPAGE_URL)))
                 .build());
 
