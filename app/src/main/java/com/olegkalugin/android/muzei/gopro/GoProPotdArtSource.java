8da79c5d73c8d407452f3fb924f45221208d4f63
Oleg Kalugin <olegspace@gmail.com>
2014-05-04 13:19:18 -0700

    Apparently each photo can have multiple url's, so check title when publishing. Also added Apache license.

--- app/src/main/java/com/olegkalugin/android/muzei/gopro/GoProPotdArtSource.java
+++ app/src/main/java/com/olegkalugin/android/muzei/gopro/GoProPotdArtSource.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2014 Oleg Kalugin
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package com.olegkalugin.android.muzei.gopro;
 
 import android.content.Intent;
@@ -40,7 +56,7 @@ public class GoProPotdArtSource extends RemoteMuzeiArtSource {
             throw new RetryException();
         }
 
-        if (getCurrentArtwork() == null || !photo.uri.equals(getCurrentArtwork().getImageUri())) {
+        if (getCurrentArtwork() == null || !photo.title.equals(getCurrentArtwork().getTitle())) {
             String token = createToken();
             publishArtwork(new Artwork.Builder()
                     .title(photo.title)
